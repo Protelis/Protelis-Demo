74f8c112983fdb22f44b87d6f6a4b6107cc56fea
Lippo <filippo.nardini@studio.unibo.it>
2019-10-29 23:37:44 +0000

    Minor change

--- 05-java-mqtt/build.gradle.kts
+++ 05-java-mqtt/build.gradle.kts
@@ -6,10 +6,9 @@ repositories {
 dependencies {
     implementation(Libs.kotlin_stdlib)
     implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.2")
+    implementation("io.moquette:moquette-broker:0.12.1")
     implementation(project(":01-java-helloworld"))
     implementation(Libs.konf)
-//    testImplementation("hu.blackbelt.bundles.moquette:io.moquette:0.11_1")
-    implementation("io.moquette:moquette-broker:0.12.1")
 }
 
 application {
