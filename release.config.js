c89fb6af8404a5e23a454ae04daa754b2c2e4650
Danilo Pianini <danilo.pianini@gmail.com>
2023-01-04 15:38:18 +0100

    ci: rebuild the CI

---
+++ release.config.js
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2010-2022, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
+var publishCmd = `
+git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md
+git push --force origin \${nextRelease.version} || exit 1
+./gradlew shadowJar --parallel || ./gradlew shadowJar --parallel || exit 2
+`
+var config = require('semantic-release-preconfigured-conventional-commits');
+config.plugins.push(
+    ["@semantic-release/exec", {
+        "publishCmd": publishCmd,
+    }],
+    ["@semantic-release/github", {
+        "assets": [ 
+            { "path": "build/libs/*-all.jar" },
+         ]
+    }],
+    "@semantic-release/git",
+)
+module.exports = config
