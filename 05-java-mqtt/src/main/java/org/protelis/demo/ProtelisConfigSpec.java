190b822279fb371d1f35da372af78b1ba55dd8bb
Danilo Pianini <danilo.pianini@unibo.it>
2024-07-30 13:00:17 +0200

    style(java-mqtt): replace explicit type arguments with a diamond

--- 05-java-mqtt/src/main/java/org/protelis/demo/ProtelisConfigSpec.java
+++ 05-java-mqtt/src/main/java/org/protelis/demo/ProtelisConfigSpec.java
@@ -20,33 +20,29 @@ public final class ProtelisConfigSpec {
      * Required field iterations. It represents the number of cycles the simulation will perform.
      */
     // CHECKSTYLE: ConstantName OFF
-    public static final RequiredItem<Integer> iterations =
-            new RequiredItem<Integer>(SPEC, "iterations") { };
+    public static final RequiredItem<Integer> iterations = new RequiredItem<>(SPEC, "iterations") { };
     /**
      * Required field protelisModuleName. It is the name of the .pt source file.
      */
     public static final RequiredItem<String> protelisModuleName =
-            new RequiredItem<String>(SPEC, "protelisModuleName") { };
+        new RequiredItem<>(SPEC, "protelisModuleName") { };
 
     /**
      * Required field brokerHost. It is the IP address of the MQTT broker.
      */
-    public static final RequiredItem<String> brokerHost =
-            new RequiredItem<String>(SPEC, "brokerHost") { };
+    public static final RequiredItem<String> brokerHost = new RequiredItem<>(SPEC, "brokerHost") { };
 
     /**
      * Required field brokerPort. It is the port of the MQTT broker.
      */
     //@SuppressWarnings("PMD.FieldNamingConventions")
     // CHECKSTYLE: ConstantName OFF
-    public static final RequiredItem<Integer> brokerPort =
-            new RequiredItem<Integer>(SPEC, "brokerPort") { };
+    public static final RequiredItem<Integer> brokerPort = new RequiredItem<>(SPEC, "brokerPort") { };
 
     /**
      * Required field nodes. It contains a list of protelis nodes.
      */
-    public static final RequiredItem<List<MqttProtelisNode>> nodes =
-            new RequiredItem<List<MqttProtelisNode>>(SPEC, "nodes") { };
+    public static final RequiredItem<List<MqttProtelisNode>> nodes = new RequiredItem<>(SPEC, "nodes") { };
 
     private ProtelisConfigSpec() { }
 }
